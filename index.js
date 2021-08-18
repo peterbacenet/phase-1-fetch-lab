326779d7b08db0bee63c606fde5c5393994b063c
Peter Bacenet <peterbacenet@icloud.com>
2021-08-17 20:17:26 -0400

    commit tree 534d57df208ac85ace0606a9f34ac0e1b3851349

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+// function fetchBooks() {
+// fetch('https://anapioficeandfire.com/api/books')
+// .then((resp) => resp.json())
+// .then(data => renderBooks(data))
+// done()
+// return renderBooks(data)
+// }
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
+
+
+///
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+    fetch("https://anapioficeandfire.com/api/books") 
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+  return fetch("https://anapioficeandfire.com/api/books")
 }
 
 function renderBooks(books) {
@@ -15,3 +41,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
